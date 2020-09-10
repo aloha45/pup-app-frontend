ab960f47c0b0d9ad82e6ad998879017571cb87fb
Cory Spicer <corys4139@gmail.com>
2020-09-09 23:02:28 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './index.css';
-import App from './App';
+import App from './pages/app/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <Router render={({history}) => <App history={history} />} />
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
